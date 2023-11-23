a954eb7b5182ac90a6c8ea0b2ededc9bc2246169
Craig Soutar <bodsda@ubuntu.com>
2023-11-23 14:14:26 +0000

    tidy up

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,11 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3" {
-    region  = "eu-west-1"
-    profile = "dis"
-    # Key, bucket and dynamodb table names are provided by the backend.conf file.
-  }
 }
 
 provider "aws" {
